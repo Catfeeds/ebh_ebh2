9a05db2f72a5352cc766f31c54522c6044c732c8
xc <309896260@qq.com>
2018-03-09 17:46:13 +0800

    Ebh首页和各个网校模板的首页登录页头部添加系统升级说明，创建通用页面

---
+++ views/common/up_header.php
@@ -0,0 +1,3 @@
+<!--系统更新提示><!-->
+<div style="background:#ff8800;font-size:14px;color:#fff;z-index: 10;height:30px;line-height:30px;width:100%;text-align:center;font-family: Microsoft Yahei;"><img style="vertical-align: text-bottom;" src="http://static.ebanhui.com/ebh/tpl/2016/images/zhuyi01.jpg" />
+亲爱的用户：为了给您提供更优质的服务，系统将于3月12号22点至3月13日7点进行升级，期间可能会出现服务中断情况，敬请谅解！</div>
